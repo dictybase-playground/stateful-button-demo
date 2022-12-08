e8127d9b9d50c4eb58298c8f50aab3822eeafdde
Siddhartha Basu <biosidd@gmail.com>
2022-12-08 06:47:38 -0600

    feat: add NormalButton component

---
+++ src/NormalButton.tsx
@@ -0,0 +1,9 @@
+import { Button, CircularProgress } from "@material-ui/core"
+
+const NormalButton = () => (
+  <Button variant="contained" color="primary">
+    <CircularProgress />
+  </Button>
+)
+
+export default NormalButton
