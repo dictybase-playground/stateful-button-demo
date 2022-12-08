1b3f8f212a33971f65e3056e96b3d41177ddac98
Siddhartha Basu <biosidd@gmail.com>
2022-12-08 10:00:41 -0600

    feat: add onClick handler to NormalButton to simulate loading

--- src/NormalButton.tsx
+++ src/NormalButton.tsx
@@ -1,9 +1,35 @@
-import { Button, CircularProgress } from "@material-ui/core"
+import { Button } from "@material-ui/core"
+import SaveIcon from "@material-ui/icons/Save"
+import { ButtonStateAtom } from "./state"
+import { useSetAtom } from "jotai"
 
-const NormalButton = () => (
-  <Button variant="contained" color="primary">
-    <CircularProgress />
-  </Button>
-)
+const successfulFetch = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("success"), 2000)
+  })
+}
+
+const NormalButton = () => {
+  const setButtonState = useSetAtom(ButtonStateAtom)
+  const onClick = async () => {
+    setButtonState("LOADING")
+    try {
+      await successfulFetch()
+      setButtonState("NORMAL")
+    } catch (error) {
+      setButtonState("ERROR")
+    }
+  }
+  return (
+    <Button
+      size="large"
+      variant="contained"
+      color="primary"
+      onClick={onClick}
+      startIcon={<SaveIcon />}>
+      {"Save"}
+    </Button>
+  )
+}
 
 export default NormalButton
