296e86fed1d2f5ea04b137c1800033b46639cc07
Siddhartha Basu <biosidd@gmail.com>
2022-12-08 05:31:56 -0600

    feat: add ErrorButton component

---
+++ src/ErrorButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@material-ui/core"
+
+const ErrorButton = () => (
+  <Button variant="contained" color="secondary">
+    {"Error"}
+  </Button>
+)
+
+export default ErrorButton
