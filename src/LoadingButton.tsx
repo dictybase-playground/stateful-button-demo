c3ec4c6187c4d645a7a2cb0644bf725c1004f3b9
Siddhartha Basu <biosidd@gmail.com>
2022-12-08 10:00:41 -0600

    feat: add onClick handler to NormalButton to simulate loading

--- src/LoadingButton.tsx
+++ src/LoadingButton.tsx
@@ -3,7 +3,7 @@ import { Button, CircularProgress } from "@material-ui/core"
 const LoadingButton = () => (
   <Button variant="contained" disabled>
     <CircularProgress />
+    {"Saving"}
   </Button>
 )
-
 export default LoadingButton
